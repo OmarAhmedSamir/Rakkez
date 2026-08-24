44df6f1904287306507b19ddcd66720f8f0eafc7
Omar Ahmed Samir <151669476+OmarAhmedSamir@users.noreply.github.com>
2026-08-24 12:55:53 +0300

    Create blog-data.js

---
+++ blog-data.js
@@ -0,0 +1,6 @@
+/* =========================================================
+   RAKKEZ BLOG DATA
+   This file is automatically updated by the RakkeZ Blog Admin.
+   ========================================================= */
+
+window.RAKKEZ_BLOG_POSTS = [];
